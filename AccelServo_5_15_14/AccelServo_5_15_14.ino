69f6daa74c76b4a17028f69888613893a1747ce7
mkiser71 <mkiser71@gmail.com>
2015-01-11 18:21:19 -0500

    Servo names
    Changed for clarity.

--- AccelServo_5_15_14/AccelServo_5_15_14.ino
+++ AccelServo_5_15_14/AccelServo_5_15_14.ino
@@ -9,41 +9,41 @@ Feel Free to distribute and alter this code!
 Using Memsic 2125 Accelerometer, Standard Hobby Servos, and Arduino Uno.
 Matches tilt from accelerometer to servos. Video at http://www.youtube.com/watch?v=i_1c97m_p0I
 */
-!!!CHANGE MOTOR NAMES TO DIRECTIONS!!!
+//!!!CHANGE MOTOR NAMES TO DIRECTIONS!!! Done - 01-11-2015
 #include <Servo.h> //Include Servo Library
 
-Servo BowBoard;  //Declares servos
-Servo SternBoard;
-Servo BowPort;
-Servo SternPort;
-int BowBoardPin = 10;  //Pin for Servos
-int SternBoardPin = 5;
-int BowPortPin = 9;
-int SternPortPin = 6;
+Servo NorthEast;  //Declares servos
+Servo SouthEast;
+Servo NorthWest;
+Servo SouthWest;
+int NorthEastPin = 10;  //Pin for Servos
+int SouthEastPin = 5;
+int NorthWestPin = 9;
+int SouthWestPin = 6;
 int xPin = 7;  //Pins for Accelerometer
 int yPin = 8;
 int sensitivity = 5;  //If > 2, will require a larger movement before activating servos
 boolean setServo = false;  //Flag for whether to change servo position or not
 int temp;  //temporary calculations value
-int BowBoardAccel = 90;  //Servo positions
-int SternBoardAccel = 180;  
-int BowPortAccel = 90;
-int SternPortAccel = 180;
+int NorthEastAccel = 90;  //Servo positions
+int SouthEastAccel = 180;  
+int NorthWestAccel = 90;
+int SouthWestAccel = 180;
 
 void setup()
 {   
   Serial.begin(9600);    // initialize serial communications
-  BowBoard.attach(BowBoardPin);  //Initializes Servos
-  SternBoard.attach(SternBoardPin);//REMOVED 5/9/14-NEED SERIAL FROM MEMSIC, NOT SERVO'S
-  BowPort.attach(BowPortPin);
-  SternPort.attach( SternPortPin);
+  NorthEast.attach(NorthEastPin);  //Initializes Servos
+  SouthEast.attach(SouthEastPin);//REMOVED 5/9/14-NEED SERIAL FROM MEMSIC, NOT SERVO'S
+  NorthWest.attach(NorthWestPin);
+  SouthWest.attach( SouthWestPin);
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
 }
 
 void loop()
 {
-  //int pulseBowBoard, pulseSternBoard, pulseBowPort, pulseSternPort;  //Resets and declares input variables
+  //int pulseNorthEast, pulseSouthEast, pulseNorthWest, pulseSouthWest;  //Resets and declares input variables
   int pulseX, pulseY;	// 5/15/14 Placed back to receive input from Memsic
   setServo = false;  //Resets flags
   digitalWrite(13, LOW);  //Turns Uno LED off
@@ -52,46 +52,46 @@ void loop()
   pulseY = (((pulseIn(yPin, HIGH) - 3698) * 18.0) / 250);
  
   
-  temp = (BowPortAccel - pulseX);  //Determines difference between new readings and current position
+  temp = (NorthWestAccel - pulseX);  //Determines difference between new readings and current position
   temp = abs(temp);
   if (temp > sensitivity)  //Checks difference to sensitivity value set above
   {
-    BowPortAccel = pulseX;
+    NorthWestAccel = pulseX;
     setServo = true;
   }
-  if (BowPortAccel > 180)  //Sets max value
+  if (NorthWestAccel > 180)  //Sets max value
   {
-    BowPortAccel = 180;
+    NorthWestAccel = 180;
     digitalWrite(13, HIGH);
   }
-  if (BowPortAccel < 0)  //Sets min value
+  if (NorthWestAccel < 0)  //Sets min value
   {
-    BowPortAccel = 0;
+    NorthWestAccel = 0;
     digitalWrite(13, HIGH);
   }
   
-  temp = (BowBoardAccel - pulseY);  //Determines difference between new readings and current position
+  temp = (NorthEastAccel - pulseY);  //Determines difference between new readings and current position
   temp = abs(temp);
   if (temp > sensitivity)  //Checks difference to sensitivity value set above
   {
-    BowBoardAccel = pulseY;
+    NorthEastAccel = pulseY;
     setServo = true;
   }
-  if (BowBoardAccel > 180)  //Sets max value
+  if (NorthEastAccel > 180)  //Sets max value
   {
-    BowBoardAccel = 180;
+    NorthEastAccel = 180;
     digitalWrite(13, HIGH);
   }
-  if (BowBoardAccel < 0)  //Sets min value
+  if (NorthEastAccel < 0)  //Sets min value
   {
-    BowBoardAccel = 0;
+    NorthEastAccel = 0;
     digitalWrite(13, HIGH);
   }
   
   if (setServo)  //Adjusts Servo Positions
   {
-    BowBoard.write(BowBoardAccel);
-    BowPort.write(BowPortAccel);
+    NorthEast.write(NorthEastAccel);
+    NorthWest.write(NorthWestAccel);
     
      // print the acceleration
   Serial.print(pulseX);
